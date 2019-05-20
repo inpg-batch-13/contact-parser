447b6173e2dfa1d78f4672c3d219cffc4c751d94
Rahmat Hidayat <kickyou44@gmail.com>
2019-05-20 21:25:43 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function getContacts(str) {
+  // your code here
+}
+
+var text = 'dimitri:dimitri@mail.com:male,icha:icha@mail.com:female,windi:windi@mail.com:female';
+console.log(getContacts(text))
+// [
+//   ['dimitri', 'dimitri@mail.com', 'male'],
+//   ['icha', 'icha@mail.com', 'female'],
+//   ['windi', 'windi@mail.com', 'female']
+// ]
