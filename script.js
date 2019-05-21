94a3a51c38a65f6125a156f09e551d5a8cb98381
felishaww <felishamiranda1510@gmail.com>
2019-05-21 21:46:27 +0700

    finally got the desired result

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function getContacts(str) {
   var splitted = split.map(function (x){
     return x.split(',')
   })
-
+  var result = splitted.map(function(i){
+    return i.join(':').split(':')
+  })
+  return result
 }
 
 var text = 'dimitri:dimitri@mail.com:male,icha:icha@mail.com:female,windi:windi@mail.com:female';
