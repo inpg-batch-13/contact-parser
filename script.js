5de547d5f0c4716ae1d1195445473db51c164202
danielevanc <danielevanc2@gmail.com>
2019-05-21 20:16:05 +0700

    step 1

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function getContacts(str) {
-  // your code here
-}
+    var splitsatu = str.split(',')
+ 
+    var splitdua =[]
+    for (i = 0 ; i < splitsatu.length ; i++){
+     splitdua[i]= new Array (splitsatu[i])
+      }
+    
+  return splitdua
+ }
+// return splitdua
+
 
 var text = 'dimitri:dimitri@mail.com:male,icha:icha@mail.com:female,windi:windi@mail.com:female';
 console.log(getContacts(text))
